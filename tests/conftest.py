c6fc1b7211224300701cf6aa59912a3d3ed0a24b
AlterAyrol <lazarev.reach@gmail.com>
2023-08-30 19:47:20 +0300

    с гитхаба

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from selene import browser, Browser
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -14,18 +14,22 @@ def web_browser():
     # browser.config.window_width = 1400
     # browser.config.window_height = 1600
     browser.config.base_url = 'https://demoqa.com'
-    capabilities = {
+    options = Options()
+    selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": False
+            "enableVideo": True
         }
     }
 
+    options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
         command_executor="https://user1%1234@selenoid:autotests.cloud/wd/hub",
-        desired_capabilities=capabilities)
+        options=options)
+
+    browser.config.driver = driver
 
     yield
 
