ee1984167c87b4127cfa2dd2191eb5ede73b8b56
AlterAyrol <lazarev.reach@gmail.com>
2023-08-30 19:42:56 +0300

    изменил на рекомендованное с selenoid UI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from selene import browser
+from selene import browser, Browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -14,21 +14,18 @@ def web_browser():
     # browser.config.window_width = 1400
     # browser.config.window_height = 1600
     browser.config.base_url = 'https://demoqa.com'
-    options = Options()
-    selenoid_capabilities = {
+    capabilities = {
         "browserName": "chrome",
         "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": True
+            "enableVideo": False
         }
     }
-    options.capabilities.update(selenoid_capabilities)
+
     driver = webdriver.Remote(
         command_executor="https://user1%1234@selenoid:autotests.cloud/wd/hub",
-        options=options)
-
-    browser.config.driver = driver
+        desired_capabilities=capabilities)
 
     yield
 
