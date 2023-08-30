d1c8d9da781908ffce99ee3e8a5b637d8733ba59
AlterAyrol <lazarev.reach@gmail.com>
2023-08-30 19:31:13 +0300

    add selenoid webdriver

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,39 @@
 import pytest
 from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
 
 
 @pytest.fixture(autouse=True)
 def web_browser():
-    driver_options = webdriver.ChromeOptions()
+    # driver_options = webdriver.ChromeOptions()
     # driver_options.add_argument('--headless')
-    browser.config.driver_options = driver_options
-    browser.config.window_width = 1400
-    browser.config.window_height = 1600
+    # browser.config.driver_options = driver_options
+    # browser.config.window_width = 1400
+    # browser.config.window_height = 1600
     browser.config.base_url = 'https://demoqa.com'
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1%1234@selenoid:autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
 
     yield
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
 
     browser.quit()
